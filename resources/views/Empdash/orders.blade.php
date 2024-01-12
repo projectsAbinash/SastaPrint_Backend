06c1073a75f3eb6070b215a0d9e005710ad04511
codexindia <sudiptobain8@gmail.com>
2024-01-12 15:09:20 +0530

    asd

--- resources/views/Empdash/orders.blade.php
+++ resources/views/Empdash/orders.blade.php
@@ -68,8 +68,8 @@
                                             @endif
                                         </td>
                                         <td class="text-center">
-                                            <a href="{{ route('emp.order.manage', ['id' => encrypt($item->order_id)]) }}"> <i
-                                                    class="fa-regular fa-eye text-primary" data-bs-toggle="tooltip"
+                                            <a href="{{ route('emp.order.manage', ['id' => encrypt($item->order_id)]) }}">
+                                                <i class="fa-regular fa-eye text-primary" data-bs-toggle="tooltip"
                                                     data-bs-placement="top" title="View And Manage Order"
                                                     style="font-size:20px;"></i></a>
                                         </td>
@@ -91,7 +91,11 @@
                                 </div>
                                 <div class="col">
                                     <div class="container p-3">
-                                       <a href="{{ route('shippinglabel',encrypt($data->id)) }}"> <i class="uil uil-newspaper d-flex justify-content-end fs-3 text-warning"></i></a>
+                                        @if ($data->status == 'placed')
+                                            <a href="{{ route('shippinglabel', encrypt($data->id)) }}"> <i
+                                                    class="uil uil-newspaper d-flex justify-content-end fs-3 text-warning"></i></a>
+                                        @endif
+
                                     </div>
 
                                 </div>
