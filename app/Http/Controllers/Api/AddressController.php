56845f21ba529d2bd02bb2c30c857a58ea951d97
codexindia <sudiptobain8@gmail.com>
2024-01-12 22:00:05 +0530

    d

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -66,7 +66,7 @@ class AddressController extends Controller
             'pincode' => 'required|numeric|digits:6'
         ]);
 
-       
+       return env('SHIPROCKET_EMAIL');
 
         //fetch pin code
         try {
